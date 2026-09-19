85120adb1c734f192f6cabc9f7a3a4802312139d
Aditya Aysola <adiays06@gmail.com>
2026-09-19 14:22:13 -0400

    changed insertion sort

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-def insertion_sort(arr: list):
-    for i in range(1, len(arr)):
-        current = arr[i]
-        inner_index = i
-        while inner_index > 0:
-            if arr[inner_index - 1] > current:
-                temp = arr[inner_index - 1]
-                arr[inner_index - 1] = current
-                arr[inner_index] = temp
-
-            inner_index -= 1
-
 def main():
     arr = [5, 3, 4, 1, 2]
-    insertion_sort(arr)
     print(arr)
 
 
