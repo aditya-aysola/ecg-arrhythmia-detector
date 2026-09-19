8fa29974147af716216c211da2618a03194ac802
Aditya Aysola <adiays06@gmail.com>
2026-09-19 14:05:19 -0400

    insertion sort testing

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
+def insertion_sort(arr: list):
+    for i in range(1, len(arr)):
+        current = arr[i]
+        inner_index = i
+        while inner_index > 0:
+            if arr[inner_index - 1] > current:
+                temp = arr[inner_index - 1]
+                arr[inner_index - 1] = current
+                arr[inner_index] = temp
+
+            inner_index -= 1
+
 def main():
-    for i in range(1, 5):
-        print("testing")
+    arr = [5, 3, 4, 1, 2]
+    insertion_sort(arr)
+    print(arr)
+
 
 if __name__ == "__main__":
     main()
