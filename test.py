a9a5278e4b311135743ff413c5d781a0a4e5af13
Aditya Aysola <adiays06@gmail.com>
2026-09-19 21:44:45 -0400

    fetching the arrhythmia data

--- test.py
+++ test.py
@@ -2,24 +2,12 @@ import math
 import random as rd
 import wfdb
 import numpy as np
-
-np.random.seed(42)
-scores = np.random.randint(50, 101, size=(20, 4))
+import matplotlib as mpl
 
 def main():
-    print("hi")
-    print(test_sum(3,1))
-    for i in range(1, 5):
-        print("testing")
-    rows, cols = scores.shape
-    print(f"Number of students: {rows}")
-    print(f"Number of exams: {cols}")
-    print(f"Overall average: {scores.mean(axis=1)}" )
+    record = wfdb.rdrecord('100', pn_dir='mitdb')
+    wfdb.plot_wfdb(record=record, title='Example signals')
 
-    # highest score
-
-def test_sum(a : int, b : int) -> int:
-    return a + b
 
 if __name__ == "__main__":
     main()
